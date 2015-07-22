e9b75e2b6b8aab124173c68380311fd4ec0ff5d2
Ryan Kennedy <ryankennedy@student-10-201-25-218.wireless.adelaide.edu.au>
2015-07-22 13:50:06 +0930

    Started working on restAdapter, which is the endpoint on android for API calls

---
+++ albertapp/app/src/main/java/cba/hackathon/albertapp/api/RestAdapter.java
@@ -0,0 +1,12 @@
+package cba.hackathon.albertapp.api;
+
+/**
+ * When you want to make an API request, instantiate this class and use the methods
+ * exposed by the instantiation.
+ * //FIXME maybe instantiate once on launch in App.java singleton...
+ */
+public class RestAdapter {
+
+
+
+}
