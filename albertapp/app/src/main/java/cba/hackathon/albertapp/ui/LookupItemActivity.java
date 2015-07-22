27a0aa1edc1f8d85cf94841f82fed192513f915f
Ravi Hammond <ravihammond@gmail.com>
2015-07-23 00:42:43 +0930

    adding working drawer, aswell as animations

--- albertapp/app/src/main/java/cba/hackathon/albertapp/ui/LookupItemActivity.java
+++ albertapp/app/src/main/java/cba/hackathon/albertapp/ui/LookupItemActivity.java
@@ -1,8 +1,14 @@
 package cba.hackathon.albertapp.ui;
 
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.NotificationCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -20,29 +26,46 @@ public class LookupItemActivity extends BaseActivity {
     private EditText mSearchInput;
     private Button mSearchBtn;
     private ListView mStockItemsList;
-    private Button mCancel;
 
     private ArrayAdapter<String> mAdapter;
-
     private App mApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lookup_item);
+        addDrawerItems();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
 
         initResources();
         setListeners();
 
         mApp = ((App) getApplicationContext());
-
         // Adding items to listview
         mAdapter = new ArrayAdapter<String>(this, R.layout.search_item, R.id.product_name, mApp.getProductList().getProductsNamesList());
         mStockItemsList.setAdapter(mAdapter);
 
-        /**
-         * Enabling Search Filter
-         * */
+    }
+
+    @Override
+    protected void initResources() {
+        mSearchInput = (EditText) findViewById(R.id.edit_search);
+        mSearchInput.setSelected(false);
+        mSearchBtn = (Button) findViewById(R.id.btn_search);
+        mStockItemsList = (ListView) findViewById(R.id.stock_items);
+    }
+
+    @Override
+    protected void setListeners() {
+        mSearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
         mSearchInput.addTextChangedListener(new TextWatcher() {
 
             @Override
@@ -62,36 +85,6 @@ public class LookupItemActivity extends BaseActivity {
             }
 
         });
-    }
-
-    @Override
-    void initResources() {
-        mSearchInput = (EditText) findViewById(R.id.edit_search);
-        mSearchInput.setSelected(false);
-        mSearchBtn = (Button) findViewById(R.id.btn_search);
-        mStockItemsList = (ListView) findViewById(R.id.stock_items);
-        mCancel = (Button) findViewById(R.id.btn_cancel);
-    }
-
-    @Override
-    void setListeners() {
-        mSearchBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        mCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                exitActivity();
-            }
-        });
-    }
-
-    private void exitActivity(){
-        finish();
     }
 
     /**
